21cce0bac13aa6f5e0b68b85e94a155242e437da
riyadsheikh370 <156985472+riyadsheikh370@users.noreply.github.com>
2024-06-04 14:53:11 +0600

    demo

--- src/components/slice/ProductSlice.jsx
+++ src/components/slice/ProductSlice.jsx
@@ -3,14 +3,23 @@ import { createSlice } from '@reduxjs/toolkit'
 export const ProductSlice = createSlice({
   name: 'product',
   initialState: {
-    cartItem: [],
+    cartItem: localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : [],
+
   },
   reducers: {
     addToCart: (state, action) => {
-let findProduct = state.cartItem.findIndex((item) => item.id == action.payload.id);
-console.log(findProduct);
+      let findProduct = state.cartItem.findIndex((item) => item.id == action.payload.id);
+      if (findProduct !== -1) {
+        state.cartItem[findProduct].qun += 1
+        localStorage.setItem("cart", JSON.stringify(state.cartItem))
+      } else {
+        state.cartItem = [...state.cartItem, action.payload]
+        localStorage.setItem("cart", JSON.stringify(state.cartItem))
+      }
     },
-
+    productIncrement: (state, action) => {
+      state.cartItem[action.payload].qun += 1
+    }
   },
 })
 
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,11 +1,72 @@
-import React from 'react'
+import React from "react";
+import { RxCross2 } from "react-icons/rx";
+import { useDispatch, useSelector } from "react-redux";
+import Container from "../components/Container";
+import Flex from "../components/Flex";
+
+
 
 const Cart = () => {
-    return (
-        <>
-            <h1>Cart</h1>
-        </>
-    )
-}
+  let dispatch = useDispatch()
+  let data = useSelector((state)=>state.product.cartItem)
 
-export default Cart
+  let handleIncrement = (index) =>{
+    dispatch(productIncrement(index))
+  }
+  return (
+    <div className="my-20">
+      <Container>
+      <h2 className="text-[#262626] font-sans text-[36px] font-bold">Cart</h2>
+      <p className="text-[#262626] font-sans text-[16px] font-normal">Home  &#62;  Cart</p>
+        <Flex className="justify-between bg-[#F5F5F3] h-[60px] items-center">
+          <div className="w-[40%]">
+            <h3 className="text-center text-[#262626] font-sans text-[20px] font-bold">Product</h3>
+          </div>
+          <div className="w-[15%]">
+          <h3 className="text-center text-[#262626] font-sans text-[20px] font-bold">Price</h3>
+          </div>
+          <div className="w-[30%]">
+          <h3 className="text-center text-[#262626] font-sans text-[20px] font-bold">Quntity</h3>
+          </div>
+          <div className="w-[15%]">
+          <h3 className="text-center text-[#262626] font-sans text-[20px] font-bold">Total</h3>
+          </div>
+        </Flex>
+
+        {data.map((item,index)=>(
+
+        <Flex className="my-14 items-center">
+          <div className="w-[40%]">
+            <div className="flex justify-around items-center">
+              <div className="">
+                    <RxCross2/>
+              </div>
+              <div className="">
+                    <img className="w-[100px] h-[100px]" src={item.thumbnail} alt="" />
+              </div>
+              <div className="">
+                <h3 className="text-[#262626] font-sans text-[16px] font-normal">{item.title}</h3>
+              </div>
+            </div>
+          </div>
+          <div className="w-[15%]">
+            <h4 className="text-center">${item.price}</h4>
+          </div>
+          <div className="w-[30%] text-center">
+          <div className="flex w-[150px] h-[50px] justify-around items-center mx-auto">
+              <div className="">-</div>
+              <div className="">{item.qun}</div>
+              <div onClick={ ()=>handleIncrement(index)} className="">+</div>
+            </div>
+          </div>
+          <div className="w-[15%]">
+            <h4 className="text-center">$44.00</h4>
+          </div>
+        </Flex>
+        ))}
+    </Container>
+    </div>
+  );
+};
+
+export default Cart;
